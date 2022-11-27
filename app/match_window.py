ab1571131e13e62e82819f4eeff11f90ba3fad98
skublin <szymonkublin@gmail.com>
2022-11-27 15:09:11 +0100

    show court image in match_window

--- app/match_window.py
+++ app/match_window.py
@@ -1,4 +1,6 @@
 import sys
+from PySide6.QtCore import Qt, QRect, QPoint
+from PySide6.QtGui import QImage, QPixmap
 from PySide6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget
 
 
@@ -15,6 +17,55 @@ class MatchWindow(QWidget):
         self.setWindowTitle(f"PyVolley - {self.team_a} vs {self.team_b}")
 
         layout = QVBoxLayout()
-        self.label = QLabel("LAYOUT LABEL")
-        layout.addWidget(self.label)
+        self.court_label = QLabel(alignment=Qt.AlignCenter)
+        layout.addWidget(self.court_label)
         self.setLayout(layout)
+
+        self.court_image = QImage()
+        self.court_image.load("C:\\Users\\szymo\\OneDrive\\Pulpit\\python\\volleyball\\app\\img\\court.jpg")
+        print(f"court img size : {self.court_image.size()}")
+        self.court_pixmap = QPixmap()
+        self.court_pixmap.convertFromImage(self.court_image)
+        self.court_label.setPixmap(self.court_pixmap)
+
+        self.court = self.court_label.rect()
+        new_top_left = QPoint(self.WIDTH / 2 - 212, self.HEIGHT / 2 - 258)
+        self.court.setTopLeft(new_top_left)
+        new_bottom_right = QPoint(self.WIDTH / 2 + 212, self.HEIGHT / 2 + 258)
+        self.court.setBottomRight(new_bottom_right)
+        print(self.court.getCoords())
+
+    def mousePressEvent(self, e):
+        if e.button() == Qt.LeftButton:
+            # handle the left-button press in here
+            print("mousePressEvent LEFT")
+
+        elif e.button() == Qt.MiddleButton:
+            # handle the middle-button press in here.
+            print("mousePressEvent MIDDLE")
+
+        elif e.button() == Qt.RightButton:
+            # handle the right-button press in here.
+            print("mousePressEvent RIGHT")
+
+    def mouseReleaseEvent(self, e):
+        if e.button() == Qt.LeftButton:
+            print("mouseReleaseEvent LEFT")
+
+        elif e.button() == Qt.MiddleButton:
+            print("mouseReleaseEvent MIDDLE")
+
+        elif e.button() == Qt.RightButton:
+            print("mouseReleaseEvent RIGHT")
+
+    def mouseDoubleClickEvent(self, e):
+        if e.button() == Qt.LeftButton:
+            print(f"(!) mouseDoubleClickEvent LEFT, position: {e.pos()} or {e.globalPos()}")
+            if self.court.contains(e.pos()):
+                print(f"INSIDE, topLeft: {self.court.topLeft()}")
+
+        elif e.button() == Qt.MiddleButton:
+            print("mouseDoubleClickEvent MIDDLE")
+
+        elif e.button() == Qt.RightButton:
+            print("mouseDoubleClickEvent RIGHT")
